90cccf14ace1e449f2a9953f6419908e9bb91d43
kyle-beatles2 <kyuewang5056@gmail.com>
2023-03-06 20:09:55 +0900

    commit commit

---
+++ acmmm23_dev/conda_envs_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pip install torch==1.7.0+cu110 torchvision==0.8.1+cu110 -f https://download.pytorch.org/whl/torch_stable.html
+conda install tqdm=4.64.1
+pip install torchsummary==1.5.1
+pip install psutil==5.9.4
+pip install matplotlib
+
+
+
+
+
